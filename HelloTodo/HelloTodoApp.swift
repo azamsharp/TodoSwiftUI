4bc916335bf80bb9a7d1b66faa1826ccb1351597
Mohammad Azam <azamsharp@gmail.com>
2021-10-13 09:10:41 -0500

    Initial Commit

---
+++ HelloTodo/HelloTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTodoApp.swift
+//  HelloTodo
+//
+//  Created by Mohammad Azam on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
